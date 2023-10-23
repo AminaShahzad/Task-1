194e23acc5c46bbbfc3f89f09656f88c606da20c
AminaShahzad <128682413+AminaShahzad@users.noreply.github.com>
2023-10-23 01:09:22 -0700

    Add files via upload

---
+++ Lab1_Q3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+float height;
+printf("Enter your height:\n");
+scanf("%f",&height);
+if (height <150){
+    printf("U are DWARF! -_-\n");
+}else if (height=150){
+    printf("U are Average -_-\n");
+}else{
+    printf("U are tall-_-\n");
+    }
+}
